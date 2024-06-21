ecaf10ca6fc6505ba9ba661fc15f6756e6a7563c
Priyanshu <priyanshu.k4444@gmail.com>
2024-06-21 11:06:51 +0530

    added achievement section in homepage

---
+++ src/widgets/Achievements.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Achievements.css";
+
+const Achievements = () => {
+  return (
+    <div className="achievements">
+      <div className="achievement-item">
+        <h2>7+</h2>
+        <p>states</p>
+      </div>
+      <div className="achievement-item">
+        <h2>100,000+</h2>
+        <p>people sensitised</p>
+      </div>
+      <div className="achievement-item">
+        <h2>250+</h2>
+        <p>children educated</p>
+      </div>
+      <div className="achievement-item">
+        <h2>1,500+</h2>
+        <p>sanitary products distributed</p>
+      </div>
+      <div className="achievement-item">
+        <h2>2</h2>
+        <p>Sushiksha schools</p>
+      </div>
+      <div className="achievement-item">
+        <h2>2</h2>
+        <p>Sushiksha schools</p>
+      </div>
+    </div>
+  );
+};
+
+export default Achievements;
--- src/widgets/Home.js
+++ src/widgets/Home.js
@@ -1,20 +1,22 @@
-import NavBar from './NavBar';
-import Impact from './Impact';
-import OurMission from './OurMission';
-import OurCompass from './OurCompass';
-import { Box } from '@mui/material';
+import NavBar from "./NavBar";
+import Impact from "./Impact";
+import OurMission from "./OurMission";
+import OurCompass from "./OurCompass";
+import { Box } from "@mui/material";
+import Achievements from "./Achievements";
 
-function Home(){
-    return(
-        <Box className='homeContainer'>
-            <Box className='navBackgroundCombo' sx={{ position: "relative" }}>
-                <NavBar />
-                <Impact />
-                <OurMission />
-                <OurCompass />
-            </Box>
-        </Box>
-    );
+function Home() {
+  return (
+    <Box className="homeContainer">
+      <Box className="navBackgroundCombo" sx={{ position: "relative" }}>
+        <NavBar />
+        <Impact />
+        <OurMission />
+        <OurCompass />
+        <Achievements />
+      </Box>
+    </Box>
+  );
 }
 
 export default Home;

7e047315cb85d0cab47466983c7c4597be889500
Priyanshu <priyanshu.k4444@gmail.com>
2024-06-25 01:39:49 +0530

    added NumberCount.js in achievements.jsx

--- src/widgets/Achievements.jsx
+++ src/widgets/Achievements.jsx
@@ -1,32 +1,46 @@
 import React from "react";
 import "./Achievements.css";
+import "./NumberCount.js";
+import NumberCount from "./NumberCount.js";
 
 const Achievements = () => {
   return (
     <div className="achievements">
       <div className="achievement-item">
-        <h2>7+</h2>
+        <h2>
+          <NumberCount value="501" suffix="+" />
+        </h2>
         <p>states</p>
       </div>
       <div className="achievement-item">
-        <h2>100,000+</h2>
+        <h2>
+          <NumberCount value="999" suffix="+" />
+        </h2>
         <p>people sensitised</p>
       </div>
       <div className="achievement-item">
-        <h2>250+</h2>
+        <h2>
+          <NumberCount value="500" suffix="+" />
+        </h2>
         <p>children educated</p>
       </div>
       <div className="achievement-item">
-        <h2>1,500+</h2>
+        <h2>
+          <NumberCount value="100" suffix="+" />
+        </h2>
         <p>sanitary products distributed</p>
       </div>
       <div className="achievement-item">
-        <h2>2</h2>
+        <h2>
+          <NumberCount value="10" suffix="+" />
+        </h2>
         <p>Sushiksha schools</p>
       </div>
       <div className="achievement-item">
-        <h2>2</h2>
-        <p>Sushiksha schools</p>
+        <h2>
+          <NumberCount value="5" suffix="+" />
+        </h2>
+        <p>Outreach Centres</p>
       </div>
     </div>
   );

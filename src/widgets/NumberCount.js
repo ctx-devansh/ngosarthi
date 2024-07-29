39201dd993f6c95fcb22bd19a6a8e8185fa0846d
Priyanshu <priyanshu.k4444@gmail.com>
2024-07-30 00:16:28 +0530

    added delay

--- src/widgets/NumberCount.js
+++ src/widgets/NumberCount.js
@@ -1,42 +1,65 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef } from "react";
 
 export default function NumberCount(props) {
-  
   // create a ref and declare an instance for each countUp animation
   const countupRef = useRef(null);
   let countUpAnim;
+
   // Use the suffix prop to add a suffix to the number
   const options = {
     suffix: props.suffix,
-  }
+  };
 
   // dynamically import and initialize countUp, sets value of `countUpAnim`
-  // you don't have to import this way, but this works best for next.js
   async function initCountUp() {
-    const countUpModule = await import('countup.js');
-    countUpAnim = await new countUpModule.CountUp(countupRef.current, props.value, options);
+    const countUpModule = await import("countup.js");
+    countUpAnim = new countUpModule.CountUp(
+      countupRef.current,
+      props.value,
+      options
+    );
     if (!countUpAnim.error) {
       countUpAnim.start();
     } else {
       console.error(countUpAnim.error);
     }
   }
- 
-  // useEffect with empty dependency array runs once when component is mounted
+
   useEffect(() => {
-    initCountUp();
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            initCountUp();
+            observer.unobserve(countupRef.current); // Stop observing after animation starts
+          }
+        });
+      },
+      { threshold: 0.5 } // Adjust this threshold as needed
+    );
+
+    if (countupRef.current) {
+      observer.observe(countupRef.current);
+    }
+
+    return () => {
+      if (countupRef.current) {
+        observer.unobserve(countupRef.current);
+      }
+    };
   }, []);
 
-
-  
   // in the jsx use the ref attribute to bind the element to `countupRef`
   return (
-    <>
-      <h1 ref={countupRef} onClick={() => {
+    <h1
+      ref={countupRef}
+      onClick={() => {
         // replay animation on click
         countUpAnim.reset();
         countUpAnim.start();
-      }}>0</h1>
-    </>
+      }}
+    >
+      0
+    </h1>
   );
 }

af29df59f5025522d661e6747d8685dbd4866843
Devansh  Gupta <devanshgupta@Devanshs-MBP.lan>
2024-07-29 11:06:32 -0700

    Started building Our Mission card and added assets and files related to it

--- src/widgets/OurMission.js
+++ src/widgets/OurMission.js
@@ -1,9 +1,17 @@
 import {Box, Typography} from '@mui/material';
  
 function OurMission(){
+
+    const ourMissionCardImgStyle = {width: "100%", height: "100%", objectFit: "cover"};
+
     return (
-        <Box sx={{height:"70vh",width:"100vw" }}>
-            <Typography variant='h6'>OurMission</Typography>
+        <Box className="ourMissionCard" sx={{height:"70vh",width:"100vw", display:"flex", flexDirection:"row" }}>
+            <Box className="ourMissionCardImg" sx={{height:"70vh",width:"35%"}}>
+                <img src="https://i.ibb.co/t3FVgzL/study.png" alt="study" border="0" style={ourMissionCardImgStyle}/>
+            </Box>
+            <Box className="ourMissionCardText" sx={{backgroundColor:"#00FF00", width:"65%"}}>
+
+            </Box>
         </Box>
     );
 }
